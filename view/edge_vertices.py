3e284597af12225db685e49a34b32412162dccca
Rafal Mi <rafal.m4@gmail.com>
2023-03-05 13:35:25 +0100

    Script listing edge vertices

--- view/edge_vertices.py
+++ view/edge_vertices.py
@@ -3,41 +3,21 @@ import bpy
 # Shift+A, Mesh -> Plane 
 
 me =  bpy.context.object.data
-ee = me.edges
-e = ee[1]
-
-print(f"Edge: {e}")
-
-e_vv = e.vertices
-
-print(f"Edge vertices: {e_vv}")
-
-e_v0 = e.vertices[0]
-e_v1 = e.vertices[1]
-
-print(f"Edge vertices: {e_v0}, {e_v1}")
-
 me_vv = me.vertices
+me_ee = me.edges
 
-for v in me_vv:
-    print(f"    {v.co}")
+print("Mesh vertices")
 
-print(f"Mesh vertices: {me_vv}")
+for (i, v) in enumerate(me_vv):
+    print(f"    {i} {v.co}")
 
-for e in ee:
+print("Mesh edge vertices")
+
+for e in me_ee:
     for i in [0, 1]:
         print(f"    {e.vertices[i]} {me_vv[e.vertices[i]].co}")
     print("")
         
-
-me_v = me_vv[0]
-
-print(f"Mesh vertex: {me_v}")
-
-# https://blender.stackexchange.com/questions/1311/how-can-i-get-vertex-positions-from-a-mesh
-co = me_v.co
-
-print(f"Vertex coordinates: {co}")
 
 
 
